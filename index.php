c5a5a71602989669cf31b90ea5ee194ac2306a6f
unknown <yassboudra@gmail.com>
2020-07-28 16:29:28 +0200

    connect to new DB

--- index.php
+++ index.php
@@ -3,20 +3,40 @@
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Farro:wght@300&display=swap" rel="stylesheet">
     <link href="./style.css" rel="stylesheet" type="text/css"/>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body class="body">
-    <form action="https://vulnerablelogin.herokuapp.com/login.php" method="POST" class="form flex">
+  
+    <form action="https://vulnerablelogin.herokuapp.com/login.php" method="POST" class="form flex" onclick="checkInput(event)" >
+    <div class="advertisment">
+        Learn Hacking <i class="green">@</i><i class="purple">virtualarity.com</i>
+    </div>
         <fieldset class="fieldset flex">
             <legend class="legend">&nbspLogin</legend>
-            <label for="email" class="label">email</label>
-            <input id="email" class="input" name="email" type="text" placeholder="Please enter your email">
-            <label for="pass" class="label">password</label>
-            <input id="pass" class="input" name="pass" type="password" placeholder="please enter your password">
-            <input class="button" type="submit" value="login">
+            <label for="email" class="label">Enter your email</label>
+            <input id="email" class="input" name="email" type="text" placeholder="email">
+            <label for="pass" class="label">Enter your password</label>
+            <input id="pass" class="input" name="pass" type="password" placeholder="password">
+            <input id="submit" class="button" type="submit" value="login">
         </fieldset>
     </form>
+    <script>
+        const email = document.getElementById("email");
+        const pass = document.getElementById("pass");
+        function checkInput(event) {
+            if(event.target.id === "submit"){
+                if(email.value === '') {
+                    event.preventDefault();
+                    email.placeholder = "Don't forget your email!" 
+                }else if(pass.value === '') {
+                    event.preventDefault();
+                    pass.placeholder = "Don't forget your password!" 
+                }
+            }
+        }
+    </script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,19 +1,19 @@
 <?php
-    $conn = mysqli_connect('localhost', 'virtualarity_com_Virtualarity', 'oeq6J8hyskUm', 'virtualarity_com_Virtualarity') or die (mysqli_connect_error);
+    $conn = mysqli_connect('localhost', 'virtualarity_com_Virtualarity', 'oeq6J8hyskUm', 'vulnerableLogin') or die (mysqli_connect_error);
 
     $email = $_POST['email'];
     $pass = $_POST['pass'];
 
-    $query = "SELECT ID FROM vulnerableLogin WHERE members.email='$email' AND members.pass='$pass'";
+    $query = "SELECT ID FROM members WHERE members.email='$email' AND members.pass='$pass'";
 
     $result = mysqli_query($conn, $query);
 
     $num = mysqli_num_rows($result);
 
     if($num !== 0) {
-        header('location: ./fd3h2fr783209ry32rh3209r87g32frfhj923f03208hf.html');
+        header('location: https://vulnerablelogin.herokuapp.com/fd3h2fr783209ry32rh3209r87g32frfhj923f03208hf.html');
     } else {
-        header('location: ./index.html');
+        header('location: https://vulnerablelogin.herokuapp.com/');
     }
 
     mysqli_close($conn);
